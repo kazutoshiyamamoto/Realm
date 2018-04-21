770b7b73fab1af126d3b873ba0fe63791710c051
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-04-21 22:39:12 +0900

    オブジェクトの生成/レコードの書き込み/レコードの読み込み

--- Realm/Realm/ViewController.swift
+++ Realm/Realm/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import RealmSwift
+
+class Person: NSObject {
+    
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+    @objc dynamic var sec = ""
+    
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let realm = try! Realm()
+        let tanaka = Person()
+
+        tanaka.name = "田中"
+        tanaka.age = 23
+        tanaka.sex = "MALE"
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func testWrite(_ sender: Any) {
+        
+        try! realm.write() {
+            realm.add(tanaka)
+        }
+    }
+    
+    @IBAction func testLoad(_ sender: Any) {
+        let realm = try! Realm()
+        realm.objects(Person.self)
+    }
+    
 }
 
